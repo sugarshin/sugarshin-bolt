7d2ede15e08c251b7539582f6497a3d74e725d5d
Shingo Sato <shinsugar@gmail.com>
2020-01-16 13:32:29 +0900

    mr-blog-cron impl

---
+++ src/crons/mr-blog/index.ts
@@ -0,0 +1,18 @@
+import { CronJob } from 'cron';
+import got from 'got';
+import { logger } from '../../initializers/logger';
+
+const { BLOG_BUILDER_CIRCLE_TOKEN: TOKEN } = process.env;
+
+export const mrBlogCron = () => {
+  const executeMonthlyReport = async () => {
+    try {
+      const res = await got.post(`https://circleci.com/api/v1/project/sugarshin/build.blog.sugarshin.net/tree/monthly-report?circle-token=${TOKEN}`);
+      logger.info(`successfully requested: ${res.statusCode} ${res.statusMessage}`);
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
+  new CronJob('0 01 00 01 * *', executeMonthlyReport).start();
+};
