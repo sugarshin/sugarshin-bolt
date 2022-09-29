00706d1aee3bbb09faee579165fa4293163930af
Shingo Sato <shinsugar@gmail.com>
2022-09-29 23:16:11 +0900

    fix ping

--- src/events/app_mention/ping/index.ts
+++ src/events/app_mention/ping/index.ts
@@ -3,8 +3,10 @@ import { logger } from '../../../initializers/logger';
 
 export const ping = (app: App): void => {
   app.event('app_mention', async ({ body, say }) => {
-    console.log('body is: ', body);
     try {
+      if (body.event.text.replace(/<@\w+>/g, '').trim() !== 'ping') {
+        return;
+      }
       await say('pong');
     } catch (error) {
       logger.error(error);
