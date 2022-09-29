8f8d381f44eece4d5e9472e9ac4233d98ac39d22
Shingo Sato <shinsugar@gmail.com>
2022-09-29 22:49:17 +0900

    tweak

--- src/events/app_mention/ping/index.ts
+++ src/events/app_mention/ping/index.ts
@@ -2,8 +2,8 @@ import { App } from '../../../types';
 import { logger } from '../../../initializers/logger';
 
 export const ping = (app: App): void => {
-  app.event('app_mention', async ({ say, message }) => {
-    console.log('message is: ', message);
+  app.event('app_mention', async ({ body, say }) => {
+    console.log('body is: ', body);
     try {
       await say('pong');
     } catch (error) {
